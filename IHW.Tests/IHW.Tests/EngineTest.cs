4d0e9eee64c823591297a06d39c2fd4b77373945
Joe Engalan <joe.engalan@gmail.com>
2016-01-18 00:34:54 -0500

    Moved tests to IHW.Tests project so that we could use Spec Flow.
    NOTE: Test project must be built before Unity tests are run to pick up latest tests.

---
+++ IHW.Tests/IHW.Tests/EngineTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace IHW.Core {
+
+	[TestFixture]
+	[Category("Core Tests")]
+	internal class EngineTest {
+
+		private Engine engine;
+
+		[SetUp]
+		public void setup() {
+			engine = new Engine();
+			engine.acceleration = 10.0f;
+			engine.maxThrust = 100.0f;
+		}
+
+		[Test]
+		public void shouldApplyPositiveThrustWhenAccelerating() {
+
+			engine.accelerate(1.0f);
+
+			Assert.That(engine.thrust, Is.EqualTo(10.0f));
+		}
+
+		[Test]
+		public void shouldApplyNegativeThrushWhenDecelerating() {
+			engine.decelerate(1.0f);
+			Assert.That(engine.thrust, Is.EqualTo(-10.0f));
+		}
+
+		[Test]
+		public void shouldNotExceedMaximumThrustWhenAccelerating() {
+			engine.accelerate(1000.0f);
+			Assert.That(engine.thrust, Is.EqualTo(engine.maxThrust));
+		}
+
+		[Test]
+		public void shouldNotExceedMaxNegativeThrustWhenDecelerating() {
+			engine.decelerate(1000.0f);
+			Assert.That(engine.thrust, Is.EqualTo(-engine.maxThrust));
+		}
+
+		[Test]
+		public void shouldStartWithZeroThrust() {
+			Assert.That (engine.thrust, Is.EqualTo(0.0f));
+		}
+
+		[Test]
+		public void shouldIncrementInitialThrustWhenAccelerating() {
+			engine.thrust = 50.0f;
+			engine.accelerate(0.5f);
+
+			Assert.That (engine.thrust, Is.EqualTo(55.0f));
+		}
+	}
+}
---
+++ IHW.Tests/IHW.Tests/TestSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace IHW.Tests
+{
+	[Binding]
+	public class TestSteps
+	{
+		Queue<int> operands = new Queue<int>();
+		
+		[Given ("I have entered (.*) into the calculator")]
+		public void GivenIHaveEnteredSomethingIntoTheCalculator (int number)
+		{
+			operands.Enqueue (number);
+		}
+
+		[When ("I press add")]
+		public void WhenIPressAdd ()
+		{
+			int op1 = operands.Dequeue ();
+			int op2 = operands.Dequeue ();
+			operands.Enqueue (op1 + op2);
+		}
+
+		[Then ("the result should be (.*) on the screen")]
+		public void ThenTheResultShouldBe (int result)
+		{
+			Assert.That (operands.Peek (), Is.EqualTo (result));
+		}
+	}
+}
+        
